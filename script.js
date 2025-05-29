59bbf4be701d3507b783f22bdd225f2cf371df29
KasiaChlopek <studio@lyofood.com>
2025-05-29 17:24:05 +0200

    9

--- script.js
+++ script.js
@@ -213,15 +213,16 @@ const mushrooms = new Mushrooms()
 
 gameplane.addEventListener('mousemove', e => {
   if(game.progress){
-    const rect = gameplane.getBoundingClientRect()
-    let newLeft = e.clientX - rect.left - (pierog.offsetWidth / 2)
+    pierog.style.left = (e.clientX - (pierog.offsetWidth / 2) ) + 'px'
+    pierog.style.top = (e.clientY - (pierog.offsetHeight / 2) ) + 'px'
   }
 })
 
 gameplane.addEventListener('touchmove', e => {
   if (game.progress && e.touches.length > 0) {
-    const rect = gameplane.getBoundingClientRect()
     const touch = e.touches[0]
+    pierog.style.left = (touch.clientX - pierog.offsetWidth / 2) + 'px'
+    pierog.style.top = (touch.clientY - pierog.offsetHeight / 2) + 'px'
   }
 })
 
